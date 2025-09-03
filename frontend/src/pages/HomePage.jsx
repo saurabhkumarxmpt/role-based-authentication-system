b9a3a695dda2c4ad0904932ed6fe2144c1270269
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-09-04 00:08:06 +0530

    include some of the taiwind css

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom";
 const HomePage=()=>{
     return(
         <>
-        <h1>this is home page</h1>
-        <button><Link to='/login'>Login</Link></button>
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6">Welcome to Role Based Auth System</h1>
+      <Link to="/login"><button className="px-6 py-2 bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-blue-700 transition">
+        Login
+      </button></Link>
+    </div>
         </>
     )
 }
