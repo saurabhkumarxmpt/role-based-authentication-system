4e12646ee58871a535f394a240d464cf5ca2e035
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-09-09 21:51:52 +0530

    do some things on the code

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
-const express = require('express'); // require use karo
+require('dotenv').config;
+const express = require('express');
+const port=process.env.PORT;
 const app = express();
 
 // middleware
@@ -10,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 // server listen
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 5000");
 });
 
