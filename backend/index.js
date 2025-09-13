a2f51e1a1a318e9d6ec46fc5c975165b2473087d
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-09-13 22:15:49 +0530

    make database file in backend

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const express=require('express');
 const app=express();
 const PORT=process.env.PORT || 5000;
+const database=require('./config/database');
 app.use(express.json());
-
+database();
 app.get('/',(req,res)=>{
     res.json({message:'This is simple server'});
 });
